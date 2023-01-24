98d05a15831b4de223086bb76d141f4c5caa16a6
enzopatriarca <enzooctaviopiccini@gmail.com>
2023-01-24 12:39:44 -0300

    users delete and edit

--- user/UserController.js
+++ user/UserController.js
@@ -42,4 +42,63 @@ router.post("/user/create",(req,res) =>{
     
 })
 
+router.get("/user/edit/:id",(req,res) =>{
+    var id = req.params.id
+    if (isNaN(id)) {
+        res.redirect("/admin/users")
+    }
+    user.findByPk(id).then(User =>{
+        if (User != undefined) {
+            res.render("admin/users/edit",{
+                User: User
+            })
+        }else{
+            res.redirect("/admin/users")
+        }
+    }).catch(erro =>{
+        res.redirect("/admin/users")
+    })
+})
+
+router.post("/user/update",(req,res) =>{
+    var id = req.body.id
+    var email = req.body.email
+    var password = req.body.password
+    user.findOne({where:{email:email}}).then(User =>{
+         if(User == undefined ){
+            user.update({email:email, password:password},{
+                where:{
+                    id:id
+                }
+            }).then(()=>{
+                res.redirect("/admin/users")
+            })
+         }else{
+            res.redirect("/admin/users")
+         }
+    })
+
+})
+
+router.post("/user/delete",(req,res) =>{
+    var id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            user.destroy({
+                where:{
+                    id:id
+                }
+            }).then(()=>{
+                res.redirect("/admin/users")
+            })
+
+        }else{
+            res.redirect("/admin/users")
+        }
+    }else{
+        res.redirect("/admin/users")
+
+    }
+})
+
 module.exports = router
